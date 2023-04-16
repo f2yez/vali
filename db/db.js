5664a55c98502b0cf736283e64372b9a1eaeeb1c
LamaNafei <87947599+LamaNafei@users.noreply.github.com>
2023-04-16 13:57:24 +0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://lamanafei:AkaiSera@cluster0.upv53lm.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://lamanafei:@cluster0.upv53lm.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
