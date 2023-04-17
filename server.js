ddecaa59927b0ec966227ab50a2388de21e7b51a
LamaNafei <lama.nafei@gmail.com>
2023-04-17 10:51:27 +0200

    use a clean and clear structure to organize your routes, and group api routes together (http://localhost/api/v1/….) and admin routes also (http://localhost/admin/…)(done)

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ const session = require('express-session');
 app.set('view engine', 'ejs');
 
 // const userRouter = require('./routes/user');
-// const adminRouter = require('./routes/admin');
+const adminRouter = require('./routes/admin');
 
 // app.use('/user', userRouter);
-// app.use('/admin', adminRouter);
 app.use(cookieParser());
-app.use(session());
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/"));
@@ -30,42 +33,50 @@ app.get('/dashboard', isAuthenticated, function(req, res) {
   res.sendFile(__dirname + "/views/dashboard.html")
 })
 
-app.get('/login', function(req, res) {
+app.get('/admin/dashboard', function(req, res) {
+  res.sendFile(__dirname + "/views/dashboard.html")
+})
+
+app.get('/api/login', function(req, res) {
   res.sendFile(__dirname + "/views/page-login.html")
 });
 
-app.post('/login', async (req, res) => {
+app.post('/api/login', async (req, res) => {
   const { email, password } = req.body;
     try {
       const user = await User.findOne({ email });
       if (!user) {
-         return res.status(400).json({ message: 'Email Not Found' });
-          // res.render('\page-login',{message: 'Email Not Found'})
+        //  return res.status(400).json({ message: 'Email Not Found' });
+          return res.render('\page-login', {message: 'Email Not Found'})
       }
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
-        return res.status(400).json({ message: 'Invalid password' });
-        // res.render('\page-login',{message: 'Invalid password'})
+        // return res.status(400).json({ message: 'Invalid password' });
+        return res.render('\page-login', {message: 'Invalid password'})
 
       }
       req.session.userID = user.userID;
       req.session.userType = user.userType;
-      return res.json({ message: 'sign in successfully' })
-      // res.render('\dashboard')
-    } 
+      if(user.userType === 1){
+        return res.render('\admin/dashboard')
+      }
+      else {
+      // return res.json({ message: 'sign in successfully' })
+      return res.render('\dashboard')
+    }
+  } 
     catch (error) {
       console.error(error);
       return res.status(500).json({ message: 'Server error' });
     }
   });
 
-
   function isAuthenticated(req, res, next) {
     if(req.session.userID){
       return next();
     }
     else {
-      res.redirect('\login');
+      res.redirect('\api/login');
     }
 }
   
